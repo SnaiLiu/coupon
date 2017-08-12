d69dd7ec8f306280d6dc6c4d1218d175d3e4bbd0
admin <liuzhen@basecity.com>
2017-08-12 17:31:48 +0800

    添加数据库操作说明

--- src/server/dao.clj
+++ src/server/dao.clj
@@ -22,17 +22,20 @@
    [[:id "SERIAL" "PRIMARY KEY"]                            ;;id
     [:name "varchar(20)" "NOT NULL"]                        ;;卡券名称
     [:description "varchar(200)"]                           ;;卡券描述
-    [:group_id "varchar(36)"]]                         ;;关联的群组
+    [:group_id "varchar(36)"]
+    [:update_time "varchar(60)"]]                         ;;关联的群组
    ;; 卡券-用户关系表
    :users_coupons
    [[:username "varchar(20)"]                           ;;用户名
     [:coupon_id "varchar(36)"]                         ;;卡券id
-    [:num "INT"]                                            ;;用户拥有的卡券数量
+    [:num "INT"]
+    [:update_time "varchar(60)"];;用户拥有的卡券数量
     ["PRIMARY KEY" "(username, coupon_id)"]]                 ;;复合主键
    ;; 群组-用户关系表
    :groups_users
    [[:group_id "varchar(36)"]
     [:username "varchar(36)"]
+    [:update_time "varchar(60)"]
     ["PRIMARY KEY" "(group_id, username)"]]
    })
 
@@ -53,17 +56,46 @@
                      ;[:id :name :description :update_time]
                      [group-id group-name group-description update-time]))
 
-(comment
-  (def mysql-db {
-                 :class-name  "com.mysql.jdbc.Driver"
-                 :subprotocol "mysql"
-                 :subname     "//localhost:3306/coupon?useUnicode=true&characterEncoding=utf8"
-                 :user        "root"
-                 :password    "111aaa"})
-  (sql/with-connection mysql-db
-                       (init-database (tables-schema)))
+(defn add-user
+  "添加一个用户"
+  [username update-time]
+  (sql/insert-rows :users [username update-time]))
 
-  (sql/with-connection mysql-db
-                       (add-group (str (UUID/randomUUID)) "柳朕&姜琳琳" "相亲相爱的一家人"
-                                  (java.sql.Timestamp. (System/currentTimeMillis))))
+(defn add-group-user
+  "添加群组-用户关系"
+  [group-id username update-time]
+  (sql/insert-rows :groups_users
+                   [group-id username update-time]))
+
+(defn mk-sql-executor
+  "创建sql执行器"
+  [sql-db]
+  (fn [sql-fn]
+    (sql/with-connection sql-db (sql-fn))))
+
+;(defn group-info-by-id
+;  "查询群组信息"
+;  [group-id]
+;  ()
+;  )
+
+(comment
+  (let [mysql-db {:class-name  "com.mysql.jdbc.Driver"
+                  :subprotocol "mysql"
+                  :subname     "//localhost:3306/coupon?useUnicode=true&characterEncoding=utf8"
+                  :user        "root"
+                  :password    "111aaa"}
+        now-time-fn (fn [] (java.sql.Timestamp. (System/currentTimeMillis)))
+        group-id (str (UUID/randomUUID))
+        exe-sql (mk-sql-executor mysql-db)]
+    ;; 创建表
+    (exe-sql #(init-database (tables-schema)))
+    ;; 添加用户
+    (exe-sql #(add-user "柳朕" (now-time-fn)))
+    (exe-sql #(add-user "姜琳琳" (now-time-fn)))
+    ;; 创建群组
+    (exe-sql #(add-group group-id "柳朕&姜琳琳" "相亲相爱的一家人" (now-time-fn)))
+    ;; 群组添加成员
+    (exe-sql #(add-group-user group-id "柳朕" (now-time-fn)))
+    (exe-sql #(add-group-user group-id "姜琳琳" (now-time-fn))))
   )
